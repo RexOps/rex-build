982825ae6d1df1257b3d4e8478d34d7fbe57ca5c
Jan <krimdomu@googlemail.com>
2014-02-15 12:05:57 +0100

    execute local

--- run.pl
+++ run.pl
@@ -47,52 +47,54 @@ else {
    $pass = $config->{box}->{default}->{password};
 }
 
-my $cwd = getcwd();
-my $rnd = get_random(8, 'a' .. 'z');
+LOCAL {
+   my $cwd = getcwd();
+   my $rnd = get_random(8, 'a' .. 'z');
 
-$ENV{"WORK_DIR"} = "/tmp/workspace/$rnd";
+   $ENV{"WORK_DIR"} = "/tmp/workspace/$rnd";
 
-mkdir "/tmp/workspace";
-mkdir "/tmp/workspace/$rnd";
-chdir "/tmp/workspace/$rnd";
+   mkdir "/tmp/workspace";
+   mkdir "/tmp/workspace/$rnd";
+   chdir "/tmp/workspace/$rnd";
 
-system 'git clone git@github.com:krimdomu/Rex.git rex --branch development >/tmp/out.log 2>&1';
+   system 'git clone git@github.com:krimdomu/Rex.git rex --branch development >/tmp/out.log 2>&1';
 
-chdir $cwd;
+   chdir $cwd;
 
-$ENV{"PATH"} = "/tmp/workspace/$rnd/rex/bin:" . $ENV{PATH};
-$ENV{"PERLLIB"} = "/tmp/workspace/$rnd/rex/lib:" . (exists $ENV{PERLLIB} ? $ENV{PERLLIB} : "");
-$ENV{"PERL5LIB"} = "/tmp/workspace/$rnd/rex/lib:" . (exists $ENV{PERL5LIB} ? $ENV{PERL5LIB} : "");
+   $ENV{"PATH"} = "/tmp/workspace/$rnd/rex/bin:" . $ENV{PATH};
+   $ENV{"PERLLIB"} = "/tmp/workspace/$rnd/rex/lib:" . (exists $ENV{PERLLIB} ? $ENV{PERLLIB} : "");
+   $ENV{"PERL5LIB"} = "/tmp/workspace/$rnd/rex/lib:" . (exists $ENV{PERL5LIB} ? $ENV{PERL5LIB} : "");
 
-system "perl /tmp/workspace/$rnd/rex/bin/rex -f contrib/prepare.rex prepare >>/var/log/rex/prepare.log 2>&1";
+   system "perl /tmp/workspace/$rnd/rex/bin/rex -f contrib/prepare.rex prepare >>/var/log/rex/prepare.log 2>&1";
 
-if($ENV{use_sudo}) {
-   system "perl /tmp/workspace/$rnd/rex/bin/rex -f contrib/prepare_sudo.rex prepare >>/var/log/rex/prepare_sudo.log 2>&1";
-   if($? != 0) {
-      print STDERR "Error preparing for sudo.\n";
-      exit 1;
+   if($ENV{use_sudo}) {
+      system "perl /tmp/workspace/$rnd/rex/bin/rex -f contrib/prepare_sudo.rex prepare >>/var/log/rex/prepare_sudo.log 2>&1";
+      if($? != 0) {
+         print STDERR "Error preparing for sudo.\n";
+         exit 1;
+      }
    }
-}
 
 
-# run tests from tests directory
-$ENV{PATH} = getcwd() . ":" . $ENV{PATH};
-system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests >junit_output_tests.xml";
+   # run tests from tests directory
+   $ENV{PATH} = getcwd() . ":" . $ENV{PATH};
+   system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests >junit_output_tests.xml";
 
 
-# run tests from tests.d directory
-opendir(my $dh, "tests.d") or die($!);
-while(my $entry = readdir($dh)) {
-   next if ($entry =~ m/^\./);
-   next if (! -d "tests.d/$entry");
+   # run tests from tests.d directory
+   opendir(my $dh, "tests.d") or die($!);
+   while(my $entry = readdir($dh)) {
+      next if ($entry =~ m/^\./);
+      next if (! -d "tests.d/$entry");
 
-   $ENV{PERL5LIB} = "tests.d/$entry/lib:" . (exists $ENV{PERL5LIB} ? $ENV{PERL5LIB} : "");
-   system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests.d/$entry >junit_output_testsd_$entry.xml";
-}
-closedir($dh);
+      $ENV{PERL5LIB} = "tests.d/$entry/lib:" . (exists $ENV{PERL5LIB} ? $ENV{PERL5LIB} : "");
+      system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests.d/$entry >junit_output_testsd_$entry.xml";
+   }
+   closedir($dh);
 
 
-system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests.post.d >junit_output_tests_post_d.xml";
+   system "WORK_DIR=$ENV{WORK_DIR} REXUSER=$user REXPASS=$pass HTEST=$ip prove --formatter TAP::Formatter::JUnit --ext rex -e rex-test tests.post.d >junit_output_tests_post_d.xml";
+};
 
 vm destroy => $new_vm;
 
