RexOps/rex-build

commit 6d74ebd00197844faa78c4099ad441d1a14b6807

install iptables

contrib/prepare.rex
@@ -14,8 +14,8 @@ my $config = Load($yaml);
 my $user = $config->{box}->{sudo}->{user};
 my $pass = $config->{box}->{sudo}->{password};
 
-user( $ENV{REX_USER}     || $config->{box}->{default}->{user} );
-password( $ENV{REX_PASS} || $config->{box}->{default}->{password} );
+user( $ENV{REXUSER}     || $config->{box}->{default}->{user} );
+password( $ENV{REXPASS} || $config->{box}->{default}->{password} );
 pass_auth;
 
 group test => split( / /, $ENV{HTEST} );
@@ -63,7 +63,7 @@ task prepare => group => test => sub {
       qr{openwrt}i     => [
       qw/block-mount coreutils-nohup perlbase-bytes perlbase-digest perlbase-essential perlbase-file perlbase-xsloader shadow-groupadd shadow-groupdel shadow-groupmod shadow-useradd shadow-userdel shadow-usermod swap-utils/
       ],
-      qr{debian|ubuntu}i => [qw/rsync augeas-tools augeas-lenses/],
+      qr{debian|ubuntu}i => [qw/rsync augeas-tools augeas-lenses iptables/],
       qr{suse}i          => [qw/lsb-release augeas augeas-lenses/],
       qr{fedora}i        => [qw/perl perl-Digest-MD5 openssh-clients which augeas augeas-libs/],
       qr{gentoo}i        => [ 'sys-process/vixie-cron', 'app-admin/augeas' ],

commit 33434713e74a0c2338b145c4fa63d50c9592a73d

fixed bug that was introduced by 6d74ebd0

contrib/prepare.rex
@@ -18,8 +18,8 @@ if ( exists $ENV{libssh2} ) {
   set connection => 'SSH';
 }
 
-user( $ENV{REXUSER}     || $config->{box}->{default}->{user} );
-password( $ENV{REXPASS} || $config->{box}->{default}->{password} );
+user( $ENV{REX_USER}     || $config->{box}->{default}->{user} );
+password( $ENV{REX_PASS} || $config->{box}->{default}->{password} );
 pass_auth;
 
 group test => split( / /, $ENV{HTEST} );