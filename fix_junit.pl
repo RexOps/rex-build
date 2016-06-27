b4372621e67a96653527b2974126be4919763a99
krimdomu <krimdomu@users.noreply.github.com>
2016-06-27 21:59:24 +0200

    add os to name

--- fix_junit.pl
+++ fix_junit.pl
@@ -5,6 +5,8 @@ use warnings;
 
 use XML::Simple;
 
+my $os = $ARGV[0];
+
 opendir(my $dh, ".") or die($!);
 while(my $entry = readdir($dh)) {
   next if($entry !~ m/\.xml$/);
@@ -14,6 +16,15 @@ while(my $entry = readdir($dh)) {
   if($ref->{testsuites}->{testsuite}->{tests} == 0) {
     $ref->{testsuites}->{testsuite}->{failures} = 1;
     $ref->{testsuites}->{testsuite}->{errors} = 1;
+    $ref->{testsuites}->{testsuite}->{"system-out"} = $ref->{testsuites}->{testsuite}->{"system-out"} . "\nNo tests run\n";
+    
+    my $new_testcase = {};
+    
+    for my $tc (keys %{ $ref->{testsuites}->{testsuite}->{testcase} }) {
+      $new_testcase->{"$os-$tc"} = $ref->{testsuites}->{testsuite}->{testcase}->{$tc};
+    }
+    
+    $ref->{testsuites}->{testsuite}->{testcase} = $new_testcase;
     
     my $o = XMLout($ref, KeepRoot => 1);
     open(my $fh, ">", $entry) or die($!);
