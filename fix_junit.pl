8abcc2233e9c0f2560bcf38e25a950b0d37df1db
krimdomu <krimdomu@users.noreply.github.com>
2016-07-10 20:11:58 +0200

    tests

--- fix_junit.pl
+++ fix_junit.pl
@@ -14,7 +14,7 @@ while(my $entry = readdir($dh)) {
   my $s = eval { local(@ARGV, $/) = ($entry); <>; };
   my $ref = XMLin($s, KeepRoot => 1, ForceArray => ['system-out', 'system-err']);
   if($ref->{testsuites}->{testsuite}->{tests} == 0) {
-    $ref->{testsuites}->{testsuite}->{failures} = 1;
+    $ref->{testsuites}->{testsuite}->{tests} = 1;
     $ref->{testsuites}->{testsuite}->{errors} = 1;
     $ref->{testsuites}->{testsuite}->{"system-out"} = $ref->{testsuites}->{testsuite}->{"system-out"} . "\nNo tests run\n";
   }
