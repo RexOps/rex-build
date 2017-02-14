ba9117813eaf1237d003c1c12f19f3ae02781f61
krimdomu <krimdomu@users.noreply.github.com>
2017-02-14 19:43:47 +0100

    use amazon

--- build/run.pl
+++ build/run.pl
@@ -8,10 +8,6 @@ use Rex::Commands::SimpleCheck;
 use Cwd 'getcwd';
 use Mojo::UserAgent;
 
-my $ua = Mojo::UserAgent->new;
-$ua->request_timeout(360);
-$ua->inactivity_timeout(360);
-
 $::QUIET = 1;
 $|       = 1;
 
@@ -19,11 +15,6 @@ my $yaml =
   eval { local ( @ARGV, $/ ) = ( $ENV{HOME} . "/.build_config" ); <>; };
 $yaml .= "\n";
 my $config = Load($yaml);
-
-my $con_str =
-    "http://$config->{jobcontrol}->{user}:$config->{jobcontrol}->{password}\@"
-  . "$config->{jobcontrol}->{host}:$config->{jobcontrol}->{port}"
-  . "/api/1.0/project/5bde00a59817c6e3e6e79cc4ad8a514a/node";
 
 chdir "..";
 
@@ -45,11 +36,11 @@ my $time   = time;
 my $new_vm = "${base_vm}-build-$time-$$";
 
 @SIG{qw( INT TERM HUP )} = sub {
-  remove_vm($new_vm);
+  remove_amazon($new_vm);
 };
 
 say "Creating VM...";
-my ( $vm_id, $ip ) = create_vm( $new_vm, $base_vm );
+my ( $vm_id, $ip ) = start_amazon( $new_vm, $base_vm );
 
 say $vm_id;
 say $ip;
@@ -58,7 +49,7 @@ say "Waiting for SSH port to be open...";
 
 my $count;
 while ( !is_port_open( $ip, 22 ) ) {
-  die "Timed out waiting for SSH" if ++$count > 60;
+  die "Timed out waiting for SSH" if ++$count > 300;
   sleep 1;
 }
 
@@ -81,100 +72,32 @@ system "REXUSER=$user REXPASS=$pass HTEST=$ip rex $ENV{rex_opts} -f build/Rexfil
 
 my $exit_code = $?;
 
-remove_vm($vm_id);
+remove_amazon($vm_id);
 
 exit $exit_code;
 
-sub create_vm {
+sub start_amazon {
+  # wait to not run into rate limit
+  #sleep (int(rand(10)) + 10);
+  my ($name, $image_id) = @_;
 
-  my ( $new_vm, $base_vm ) = @_;
-  my ( $ip, $vm_id );
-
-  if ( $ENV{use_docker} ) {
-
-    my $tx = $ua->post(
-      $con_str,
-      json => {
-        name => $new_vm,
-        type => "docker",
-        parent =>
-          "3a7f1fc9e58a8492fc625d8a16e85e76_c5fd214cdd0d2b3b4272e73b022ba5c2",
-        data => {
-          image => $base_vm,
-          host =>
-"3a7f1fc9e58a8492fc625d8a16e85e76_1b21b0d71706897b69f108572c444d40_b0da275520918e23dd615e2a747528f1",
-          command => "/usr/sbin/sshd -D",
-        }
-      }
-    );
-
-    if ( $tx->success ) {
-      my $ref = $tx->res->json;
-
-      $vm_id = $ref->{id};
-      if ( !$vm_id ) {
-        die "Error creating test VM";
-      }
-
-      my $qtx = $ua->get("$con_str/$vm_id");
-      if ( $qtx->success ) {
-        my $qref = $qtx->res->json;
-        $ip = $qref->{provisioner}->[0]->{NetworkSettings}->{IPAddress};
-      }
-      else {
-        die "Error getting info of test VM";
-      }
+  my $image_id = shift;
+  my $vm = cloud_instance create => {
+    image_id       => $image_id,
+    name           => $name,
+    key            => "integration-tests",
+    type           => $instance_type,
+    security_group => 'default',
+    options        => {
+      SubnetId => 'subnet-0ac1f97d',
     }
-    else {
-      die "Error creating test VM";
-    }
+  };
 
-  }
-  else {
-
-    my $tx = $ua->post(
-      $con_str,
-      json => {
-        name => $new_vm,
-        type => "kvm",
-        parent =>
-          "3a7f1fc9e58a8492fc625d8a16e85e76_c5fd214cdd0d2b3b4272e73b022ba5c2",
-        data => {
-          image => $base_vm,
-          host =>
-"3a7f1fc9e58a8492fc625d8a16e85e76_1b21b0d71706897b69f108572c444d40_b0da275520918e23dd615e2a747528f1",
-        }
-      }
-    );
-
-    if ( $tx->success ) {
-      my $ref = $tx->res->json;
-
-      $vm_id = $ref->{id};
-      if ( !$vm_id ) {
-        die "Error creating test VM";
-      }
-
-      my $qtx = $ua->get("$con_str/$vm_id");
-      if ( $qtx->success ) {
-        my $qref = $qtx->res->json;
-        $ip = $qref->{provisioner}->{network}->[0]->{ip};
-      }
-      else {
-        print STDERR Dumper $qtx;
-        die "Error getting info of test VM";
-      }
-    }
-    else {
-      die "Error creating test VM";
-    }
-
-  }
-
-  return ( $vm_id, $ip );
+  return ( $vm->{id}, $vm->{ip} );
 }
 
-sub remove_vm {
-  my ($vm_id) = @_;
-  $ua->delete("$con_str/$vm_id");
+sub remove_amazon {
+  my $instance_id = shift;
+  cloud_instance terminate => $instance_id;
 }
+
